1346cb68408635e55f5fecdbfad03b42f28f1e09
Aabid Hasan <aabidhasan495@gmail.com>
2024-05-27 07:28:56 +0545

    .

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useParams } from 'react-router'
+
 import './details.scss'
+import useFetch from '../../hooks/useFetch'
+import DetailsBanner from './detailsBanner/DetailsBanner'
+
+
 
 const Details = () => {
+
+  const {mediaType, id} = useParams();
+  const {data, loading} = useFetch(`/${mediaType}/${id}/videos`);
+  const {data:credits, loading:creditsLoading} = useFetch(`/${mediaType}/${id}/credits`);
+
   return (
     <div>
-      Details
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
     </div>
   )
 }
